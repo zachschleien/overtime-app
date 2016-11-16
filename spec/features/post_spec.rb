857424ad1a21c7df2f14bf555400785a36623aad
zachschleien <zschleien@gmail.com>
2016-11-15 21:04:11 -0500

    Ability to add new posts with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,15 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from the home page' do
+      visit root_path
+
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
@@ -50,6 +59,29 @@ describe 'navigate' do
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
+  describe 'edit' do
+    before do
+      @post = FactoryGirl.create(:post)
+    end
+
+    it 'can be reached by clicking edit on index page' do
+      visit posts_path
+
+      click_link("edit_#{@post.id}")
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be edited' do
+      visit edit_post_path(@post)
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Edited content"
+      click_on "Save"
+
+      expect(page).to have_content("Edited content")
     end
   end
 end
